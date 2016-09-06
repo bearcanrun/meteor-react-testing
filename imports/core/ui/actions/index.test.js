c7f99d45b53c6e2b00c957c9287f96e082ad7813
Barry Low <barrylow@gmail.com>
2016-09-06 12:53:45 -0700

    Add core action tests

--- imports/core/ui/actions/index.test.js
+++ imports/core/ui/actions/index.test.js
@@ -2,19 +2,21 @@ import { describe, it, expect } from 'mocha';
 import configureStore from 'redux-mock-store';
 import thunk from 'redux-thunk';
 
+import { actions as coreActions } from './index';
+
 const middlewares = [thunk];
 const mockStore = configureStore(middlewares);
 
 describe('core actions', () => {
   it('should dispatch', () => {
     const initialState = {};
-    const addTodo = { type: 'ADD_TODO' };
+    const clearMessages = coreActions.clearMessages;
 
     const store = mockStore(initialState);
-    store.dispatch(addTodo);
+    store.dispatch(clearMessages);
 
     const actions = store.getActions();
 
-    expect(actions).toEqual([addTodo]);
+    expect(actions).toEqual([clearMessages]);
   });
 });
